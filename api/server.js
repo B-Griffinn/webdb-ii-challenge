790421e58847b3ecf0ec2e31b86993b98a51bdc2
Ben <bigriffin94@outlook.com>
2019-10-08 15:23:41 -0700

    IT'S WORKING --> CRUD OPPS

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 
 // Import my router when it has been created
+const carsRouter = require('../router(s)/cars-router.js');
 
 const server = express();
 
@@ -9,5 +10,6 @@ server.use(helmet());
 server.use(express.json());
 
 // use my router on the server once created
+server.use('/api/cars', carsRouter);
 
 module.exports = server;
--- router(s)/cars-router.js
+++ router(s)/cars-router.js
@@ -1,8 +1,31 @@
 const express = require('express');
-const knex = require('knex');
-
+const router = express.Router();
 const knexConfig = require('../knexfile.js');
-
+const knex = require('knex');
 const db = knex(knexConfig.development);
 
-const router = express.Router();
+router.get('/', (req, res) => {
+    db('cars')
+    .select()
+    .then(carsList => {
+        res.status(200).json(carsList)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'There was an error retrieving that list of cars.' })
+    })
+});
+
+
+router.post('/', (req, res) => {
+    db('cars') // locate our cars table in the db
+    .insert(req.body)
+    .then(update => {
+        res.status(201).json(update)
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'There was an issue creating that car.'})
+    })
+});
+
+
+module.exports = router;
