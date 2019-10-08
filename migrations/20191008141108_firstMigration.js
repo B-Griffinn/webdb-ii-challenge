676571015148315f61bf923fe92b973a369e9a5c
Ben <bigriffin94@outlook.com>
2019-10-08 14:35:51 -0700

    initial migration created - tables and schema

--- migrations/20191008141108_firstMigration.js
+++ migrations/20191008141108_firstMigration.js
@@ -1,6 +1,32 @@
-
+/*
+knex.schema.createTable('users', function (table) {
+  table.increments();
+  table.string('name');
+  table.timestamps();
+})
+ */
 exports.up = function(knex) {
-  
+  knex.schema.createTable('cars', function(tbl) {  
+    tbl.increments();
+
+    tbl.integer('vin', 17);
+    notNullable();
+
+    tbl.string('make', 128)
+    .notNullable();
+
+    tbl.string('model', 128)
+    .notNullable();
+
+    tbl.string('mileage', 6)
+    .notNullable();
+
+    tbl.string('transmission', 128)
+    .nullable();
+
+    tbl.string('status', 128)
+    .nullable();
+  })
 };
 
 exports.down = function(knex) {
