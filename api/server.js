b6cbe590b318bb4b8c4b0a47ed6fdea967ce1ef9
Ben <bigriffin94@outlook.com>
2019-10-08 12:51:31 -0700

    index.js and server created. Server is running properly

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const helmet = require('helmet');
 
+// Import my router when it has been created
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
+// use my router on the server once created
 
 module.exports = server;
